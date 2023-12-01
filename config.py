ffebac65e38a7e10ace755ca44e34b081f30c4df
aleksandra <aleksaantsupova@yandex.ru>
2023-12-01 16:53:46 +0300

    1

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-SENDER = 'aleksaantsupova@yandex.ru'
-RECEIVERS = ['st077009@student.spbu.ru']
-SENDER_NAME = 'TEST <aleksaantsupova@yandex.ru>'
-MAIL_THEME = 'Хранители русской культуры'
+SENDER = 'cbs.forum@mail.ru'
+# RECEIVERS = ['aleksaantsupova@yandex.ru']
+SENDER_NAME = 'CBS Forum <cbs.forum@mail.ru>'
+MAIL_THEME = 'Новые Бибилиотекари 5.0'
+
--- main.py
+++ main.py
@@ -3,27 +3,45 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.header import Header
 import config
+import pandas as pd
 
-def main():
-    msg = MIMEMultipart()
-    msg.set_charset('utf-8')
 
-    msg['From'] = config.SENDER_NAME
-    msg['To'] = ', '.join(config.RECEIVERS)
-    msg['Subject'] = config.MAIL_THEME
+def get_receivers():
+    df = pd.read_excel('mail.xlsx')
+    return list(df['email'])
 
-    with open('letter.html', 'r') as file:
-        html = file.read()
-        body = MIMEText(html, 'html')
-        msg.attach(body)
 
-    smtpObj = smtplib.SMTP('smtp.yandex.ru', 587)
+def main(receivers):
+    smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
     smtpObj.starttls()
 
-    smtpObj.login(config.SENDER, 'p23LRvst')
-    smtpObj.sendmail(config.SENDER_NAME, config.RECEIVERS, msg.as_string())
+    smtpObj.login(config.SENDER, 'xebjzzLUNbzQgT7v9yCB')
+    i = 0
+    for re in receivers:
+        i += 1
+        print(i)
+        try:
+            msg = MIMEMultipart()
+            msg.set_charset('utf-8')
+            msg['From'] = config.SENDER_NAME
+            msg['Subject'] = config.MAIL_THEME
+            msg['To'] = re
+            with open('letter.html', 'r') as file:
+                html = file.read()
+                body = MIMEText(html, 'html')
+                msg.attach(body)
+
+            smtpObj.sendmail(config.SENDER_NAME, [re], msg.as_string())
+            with open('success1.txt', 'a') as success_file:
+                success_file.write(re + '\n')
+        except Exception:
+            with open('not_sended.txt', 'a') as not_sended_file:
+                not_sended_file.write(re + '\n')
     print("Successfully sent email")
     smtpObj.quit()
 
+
 if __name__ == '__main__':
-    main()
+    # main(['st077009@student.spbu.ru', 'svsha.ivanova@dsahejkl12jk'])
+
+    main(get_receivers())
